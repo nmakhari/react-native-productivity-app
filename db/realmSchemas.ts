0e7f6e5ad00ce50d60fb560ac39afa409f1fe518
Nick Makharinets <nmakhari@uwaterloo.ca>
2021-01-23 11:46:15 -0500

    Implemented all schemas as classes

--- db/realmSchemas.ts
+++ db/realmSchemas.ts
@@ -29,6 +29,27 @@ export class GroupTodo extends Realm.Object {
       },
     },
   };
+
+  id: number;
+  name: string;
+  done: boolean;
+  points: number;
+  group: Group;
+
+  constructor(
+    id: number,
+    name: string,
+    points: number,
+    group: Group,
+    done?: boolean | null,
+  ) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.done = done ?? false;
+    this.points = points;
+    this.group = group;
+  }
 }
 
 export class ReadingTodo extends Realm.Object {
@@ -49,6 +70,34 @@ export class ReadingTodo extends Realm.Object {
       },
     },
   };
+
+  id: number;
+  name: string;
+  pageStart: number;
+  pageEnd: number;
+  done: boolean;
+  reading: Reading;
+
+  constructor(
+    id: number,
+    name: string,
+    pageStart: number,
+    pageEnd: number,
+    reading: Reading,
+    done?: boolean | null,
+  ) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.pageStart =
+      pageStart >= 0 && pageStart < reading.pagesTotal ? pageStart : 0;
+    this.pageEnd =
+      pageEnd <= reading.pagesTotal && pageEnd > 0
+        ? pageEnd
+        : reading.pagesTotal;
+    this.done = done ?? false;
+    this.reading = reading;
+  }
 }
 
 export class Todo extends Realm.Object {
@@ -61,6 +110,17 @@ export class Todo extends Realm.Object {
       done: {type: 'bool', default: false},
     },
   };
+
+  id: number;
+  name: string;
+  done: boolean;
+
+  constructor(id: number, name: string, done?: boolean | null) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.done = done ?? false;
+  }
 }
 
 export class Reading extends Realm.Object {
@@ -76,6 +136,30 @@ export class Reading extends Realm.Object {
       readings: {type: 'list', objectType: SchemaNames.READING_TODO_SCHEMA}, // to many relationship
     },
   };
+
+  id: number;
+  name: string;
+  creationDate: Date;
+  pagesComplete: number;
+  pagesTotal: number;
+  readings?: Realm.List<ReadingTodo & Realm.Object> | null;
+
+  constructor(
+    id: number,
+    name: string,
+    creationDate: Date,
+    pagesTotal: number,
+    pagesComplete?: number | null,
+    readings?: Realm.List<ReadingTodo & Realm.Object> | null,
+  ) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.creationDate = creationDate;
+    this.pagesComplete = pagesComplete ?? 0;
+    this.pagesTotal = pagesTotal;
+    this.readings = readings;
+  }
 }
 
 export class Group extends Realm.Object {
@@ -87,13 +171,37 @@ export class Group extends Realm.Object {
       name: 'string',
       creationDate: 'date',
       description: 'string?',
+      pointTotal: 'int',
       items: {type: 'list', objectType: SchemaNames.GROUP_TODO_SCHEMA}, // to many relationship
     },
   };
+
+  id: number;
+  name: string;
+  creationDate: Date;
+  description?: string | null;
+  pointTotal: number;
+  items?: Realm.List<GroupTodo & Realm.Object> | null;
+
+  constructor(
+    id: number,
+    name: string,
+    creationDate: Date,
+    description?: string | null,
+    items?: Realm.List<GroupTodo & Realm.Object> | null,
+  ) {
+    super();
+    this.id = id;
+    this.name = name;
+    this.pointTotal = items?.sum('points') ?? 0;
+    this.creationDate = creationDate;
+    this.description = description;
+    this.items = items;
+  }
 }
 
 export class TodoList extends Realm.Object {
-  public static schema: Realm.ObjectSchema = {
+  static schema: Realm.ObjectSchema = {
     name: SchemaNames.TODO_LIST_SCHEMA,
     properties: {
       creationDate: 'date',
@@ -103,10 +211,10 @@ export class TodoList extends Realm.Object {
     },
   };
 
-  public creationDate: Date;
-  public groups?: Realm.List<Group & Realm.Object> | null;
-  public readings?: Realm.List<Reading & Realm.Object> | null;
-  public items?: Realm.List<Todo & Realm.Object> | null;
+  creationDate: Date;
+  groups?: Realm.List<Group & Realm.Object> | null;
+  readings?: Realm.List<Reading & Realm.Object> | null;
+  items?: Realm.List<Todo & Realm.Object> | null;
 
   constructor(
     creationDate: Date,
